8617a0f986a4191ef7e9abdad29f0b4364b82328
xcv135135 <xcv135135135@gmail.com>
2020-02-07 09:16:38 +0000

    adjust convert.js which exec the shell command to convert file format

--- server/convert.js
+++ server/convert.js
@@ -2,7 +2,8 @@ var shell = require('shelljs');
 
 var video_filename = 'input';
 var convert = function (input_filename) {
-	shell.exec('ffmpeg -i input.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls videoinput/output.m3u8');
+	let command = 'ffmpeg -i ' + video_filename + '.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls video' + video_filename + '/output.m3u8';
+	shell.exec(command);
 }
 
 convert(video_filename);
